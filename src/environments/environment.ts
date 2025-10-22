1e916ce1c22b7dc9a1cc955f2ebbd494180fd0a3
Erysnell <erysnell09@gmail.com>
2025-10-22 11:12:52 -0400

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs favoritos',
+
+  // ApiKeys
+  // https://developers.giphy.com/dashboard/
+  giphyApiKey: 'INSERTA TU API_KEY',
+  giphyUrl: 'https://api.giphy.com/v1',
+};
